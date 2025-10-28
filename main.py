8516edc470b0a422bd4950fe06276e1be98f651b
matheus santos <150365502+matheusssi@users.noreply.github.com>
2025-10-28 20:20:38 -0300

    rotas ajustadas e main

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from fastapi import FastAPI
 
+# Importações de rotas existentes
 from api.routes.empresa_routes import router as empresa_router
 from api.routes.tipo_routes import router as tipo_router
 from api.routes.fornecedor_routes import router as fornecedor_router
 
+from api.routes.produto_routes import router as produto_router
+from api.routes.estoque_routes import router as estoque_router
+
 app = FastAPI(title = "TESTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+
 
 app.include_router(empresa_router)
 app.include_router(tipo_router)
 app.include_router(fornecedor_router)
 
+
+app.include_router(produto_router)
+app.include_router(estoque_router)
+
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-
-
